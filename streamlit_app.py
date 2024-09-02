d76a10abb2d1241ece56b58bde78c6d479fbe6c1
sxc-morcus <60882922+sxc-morcus@users.noreply.github.com>
2024-09-02 09:54:02 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
+import pandas as pd
 
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+df = pd.read_csv("dat.csv")
+st.line_chart(df)
