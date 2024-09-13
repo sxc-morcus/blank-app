ff16fed78ca9e98fd8a833d8a074ee48f17d688c
sxc-morcus <60882922+sxc-morcus@users.noreply.github.com>
2024-09-13 11:18:56 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,11 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title='My Super App',
+    page_icon=':ice_cream:',
+)
+
+
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
