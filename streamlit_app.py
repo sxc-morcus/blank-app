6ea159f6ecafb7f832000a3aa413bb3c5e0b82ae
sxc-morcus <60882922+sxc-morcus@users.noreply.github.com>
2024-09-22 12:46:50 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,8 +68,8 @@ my_xticks = ['one','two','three','four', 'five']
 
 fig = plt.figure()
 
-#plt.plot(counts[0])
+plt.plot(counts['Democrat'], counts[Income])
 
-#st.pyplot(plt)
+st.pyplot(plt)
 
 st.write(counts)
